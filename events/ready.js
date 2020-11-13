7a7fb1a9d37ee228596b4800c6af76cf83c9d557
Glitch (pinnate-dust-dandelion) <none>
2020-11-13 13:18:39 +0000

    🍢🐫 Updated with Glitch

---
+++ events/ready.js
@@ -0,0 +1,29 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+var oyun = [
+        "Prefix '+' ",
+        "PXONS BOT EMRİNİZDE 😈",
+        "Sorular İçin Marull Admine",
+        + client.guilds.size + " Sunucu " | + client.users.size + " Kullanıcı "
+    ];
+
+   setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "https://www.twitch.tv/Pxons0" );
+        }, 2 * 2500); 
+    
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar başarıyla yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: PXONSBot başarıyla aktif edildi! Sunucuda küfür,reklam vb. şeyler engellenecektir.`);
+  client.user.setStatus("online");
+  client.user.setActivity(`${prefix} ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
